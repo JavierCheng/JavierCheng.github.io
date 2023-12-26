bcb01ffd502093a58ea4dd6962b0c7b7ba733d82
JavierCheng <138783535+JavierCheng@users.noreply.github.com>
2023-12-26 16:31:50 +0800

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -9,3 +9,39 @@ window.onload = function() {
         }
     }
 }
+//checks if the browser supports web storage
+function check_web_storage_support() {
+    if(typeof(Storage) !== "undefined") {
+        return(true);
+    }
+    else {
+        alert("Web storage unsupported!");
+        return(false);
+    }
+}
+//retrieves the saved note
+function display_saved_note() {
+    if(check_web_storage_support() == true) {
+        result = localStorage.getItem('note');
+    }
+    if(result === null) {
+        result = "No note saved";
+    }
+    document.getElementById('area').value = result;
+}
+//implements the functionality to save a note
+function save() {
+    if(check_web_storage_support() == true) {
+        var area = document.getElementById("area");
+        if(area.value != '') {
+            localStorage.setItem("note", area.value);
+        }
+        else {
+            alert("Nothing to save");
+        }
+    }
+}
+//clears the text screen
+function clear() {
+    document.getElementById('area').value = "";
+}
