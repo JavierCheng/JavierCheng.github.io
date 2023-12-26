21a826772f47e632efa7ee7767043de616b3ccc0
JavierCheng <138783535+JavierCheng@users.noreply.github.com>
2023-12-26 16:34:50 +0800

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -19,6 +19,15 @@ window.onload = function() {
     p.appendChild(text);
 }
     }
+    var home = document.getElementById('container').innerHTML;
+var controls = document.getElementById('controls').innerHTML;
+display_saved_note();
+document.getElementById('home').onclick = function() {
+    document.getElementById('container').innerHTML = home;
+    document.getElementById('controls').innerHTML = controls;
+    document.getElementById('menu').className = 'hiddenmenu';
+    display_saved_note();
+}
 }
 //checks if the browser supports web storage
 function check_web_storage_support() {
